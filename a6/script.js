020309814d1d37ac9f506f49f92371a299519337
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-12 20:48:38 +0530

    update

---
+++ a6/script.js
@@ -0,0 +1,12 @@
+const userInput = document.getElementById('user');
+const passInput = document.getElementById('pass');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+  const user = userInput.value;
+  const pass = passInput.value;
+
+  const output = document.createElement('p');
+  output.textContent = ${user}, ${pass};
+  document.body.appendChild(output);
+});
