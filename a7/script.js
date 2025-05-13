36dd0dd4eea27bfe0af03cec9f0e9d7d661e6326
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-13 19:50:05 +0530

    update

--- a7/script.js
+++ a7/script.js
@@ -2,15 +2,12 @@ function fun() {
     let n1 = parseFloat(document.getElementById("n1").value);
     let n2 = parseFloat(document.getElementById("n2").value);
 
-    let add = n1 + n2;
-    let sub = n1 - n2;
-    let mul = n1 * n2;
-    let div = n1 / n2;
-
-    document.body.innerHTML += `
-        <p><strong>Addition:</strong> ${add}</p>
-        <p><strong>Subtraction:</strong> ${sub}</p>
-        <p><strong>Multiplication:</strong> ${mul}</p>
-        <p><strong>Division:</strong> ${div}</p>
+    let resultHTML = `
+        <p>Addition: ${n1 + n2}</p>
+        <p>Subtraction: ${n1 - n2}</p>
+        <p>Multiplication: ${n1 * n2}</p>
+        <p>Division: ${n1 / n2}</p>
     `;
+
+    document.getElementById("result").innerHTML = resultHTML;
 }
