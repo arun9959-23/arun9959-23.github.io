62979593d739b5fa8577654b7c85df9c349a770d
arun9959-23 <23r11a6710@gcet.edu.in>
2025-05-17 18:49:15 +0530

    update

---
+++ a14/script.js
@@ -0,0 +1,11 @@
+const items=[
+    {name:"apple",price:50,status:"pending"},
+    {name:"banana",price:30,status:"pending"},
+    {name:"orange",price:80,status:"pending"},
+
+];
+
+const updateItems=items.map((item)=>({
+    ...item,price:item.price+5,item_status:"complete"
+
+}))
